1ece2802bfa26333a02dfb4a8c80209c981dded9
JohnJohnMan <jjm4248@gmail.com>
2024-10-14 01:38:21 +0900

    main 페이지 볼륨 증가와 sports, musical, classic, concert 카테고리 컨텐츠 추가 및 보완

--- src/main/java/org/example/moreticket/controller/PageController.java
+++ src/main/java/org/example/moreticket/controller/PageController.java
@@ -9,13 +9,37 @@ public class PageController {
     // 스포츠 카테고리 선택 시 띄우는 예메 메인 페이지
     @GetMapping("/Sports")
     public String Sports() {
-        return "Sports";
+        return "Category/Sports";
     }
 
-    // 원하는 경기 선택하고 띄우는 예매 페이지
-    @GetMapping("/firstsportsmatch")
-    public String firstSportsMatch() {
-        return "firstsportsmatch";
+    @GetMapping("/Concert")
+    public String Concert() {
+        return "Category/Concert";
     }
+
+    @GetMapping("/Musical")
+    public String Musical() {
+        return "Category/Musical";
+    }
+
+    @GetMapping("/Classic")
+    public String Classic() {
+        return "Category/Classic";
+    }
+
+
+
+
+    @GetMapping("/inf")
+    public String inf() { return "Inf/MusicalInf"; }
+
+    @GetMapping("/inf2")
+    public String inf2() { return "Inf/ConcertInf"; }
+
+    @GetMapping("/inf3")
+    public String inf3() { return "Inf/SportsInf"; }
+
+    @GetMapping("/inf4")
+    public String inf4() { return "Inf/ClassicInf"; }
 
 }
--- src/main/resources/static/script/main.js
+++ src/main/resources/static/script/main.js
@@ -58,3 +58,45 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+function showEvents(listId) {
+    // 모든 이벤트 리스트 숨기기
+    var eventLists = document.querySelectorAll('.event-list');
+    eventLists.forEach(function (list) {
+        list.style.display = 'none';  // 리스트를 숨길 때 'none'으로 처리
+    });
+
+    // 선택한 리스트만 보이도록 설정
+    var selectedList = document.getElementById(listId);
+    selectedList.style.display = 'flex';  // 리스트를 보일 때 'flex'로 처리
+}
+
+function showVideos(listId) {
+    // 모든 이벤트 리스트 숨기기
+    var videoLists = document.querySelectorAll('.video-list');
+    videoLists.forEach(function (list) {
+        list.style.display = 'none';  // 리스트를 숨길 때 'none'으로 처리
+    });
+
+    // 선택한 리스트만 보이도록 설정
+    var selectedList = document.getElementById(listId);
+    selectedList.style.display = 'flex';  // 리스트를 보일 때 'flex'로 처리
+}
+
+function selectItem(itemId) {
+    // 선택한 경기 ID를 URL 파라미터로 전송
+    if (itemId === 1 || itemId === 2 || itemId === 3 || itemId === 33) {
+        location.href = '/inf?itemId=' + itemId;
+    } else if (itemId === 4 || itemId === 5 || itemId === 6) {
+        location.href = '/inf2?itemId=' + itemId;
+    } else if (itemId === 7 || itemId === 8 || itemId === 9) {
+        location.href = '/inf3?itemId=' + itemId;
+    } else if (itemId === 10 || itemId === 11 || itemId === 12 || itemId === 13) {
+        location.href = '/inf4?itemId=' + itemId;
+    }
+}
+
+
+
+
+
